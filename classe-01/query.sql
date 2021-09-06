c9d6a2f759e3ba74716b62371edddbbc222a3181
JeanSisse <tavares1988@live.com>
2021-09-06 07:19:31 -0300

    Selecione todos os registros com todos os campos da tabela 'musicas', onde o compositor não seja nulo, o tempo seja menor que 5 minutos e o nome do compositor seja diferente de 'Bach', resolvido

--- classe-01/query.sql
+++ classe-01/query.sql
@@ -4,3 +4,5 @@ SELECT compositor, id FROM musicas;
 SELECT composicao, tempo FROM musicas WHERE tempo > 240;
 
 SELECT compositor, composicao FROM musicas WHERE id BETWEEN 47 AND 123;
+
+SELECT * FROM musicas WHERE compositor IS NOT NULL AND tempo > 300 AND compositor <> 'Bach';
